a05c002d693c740ebab65a743e874a09ae2c5c6e
unknown <mahendrathota131213@gmail.com>
2020-04-20 09:02:59 +0530

    modified for sonar qube

--- src/main/java/edu/ucsb/cs56/w16/sparkjava/SparkHello.java
+++ src/main/java/edu/ucsb/cs56/w16/sparkjava/SparkHello.java
@@ -11,8 +11,29 @@ get("/hello", (req, res) -> "Hello World");
 int a=10;
 int b=10;
 int c=a+b;
-c=a+b;
-c=a+b;
+int d=a-b;
+  BufferedReader br = new BufferedReader(new FileReader("C:\\test.txt"));
+        System.out.println(br.readLine());
+
+System.out.println("Addition is "+c);
+system.out.println("substraction is "+d);
+
+String ptr = null; 
+  
+        // Checking if ptr.equals null or works fine. 
+        try
+        { 
+            // This line of code throws NullPointerException 
+            // because ptr is null 
+            if (ptr.equals("gfg")) 
+                System.out.print("Same"); 
+            else 
+                System.out.print("Not Same"); 
+        } 
+        catch(NullPointerException e) 
+        { 
+            System.out.print("NullPointerException Caught"); 
+        } 
     }
 
 }
