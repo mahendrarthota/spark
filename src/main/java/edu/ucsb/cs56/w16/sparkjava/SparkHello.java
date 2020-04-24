2012ff6865f56572e18a3ba20adcc65e34ac27fd
unknown <mahendrathota131213@gmail.com>
2020-04-24 13:24:52 +0530

    message

--- src/main/java/edu/ucsb/cs56/w16/sparkjava/SparkHello.java
+++ src/main/java/edu/ucsb/cs56/w16/sparkjava/SparkHello.java
@@ -8,6 +8,16 @@ public class SparkHello {
     public static void main(String[] args) {
         
 get("/hello", (req, res) -> "Hello World");
+System.out.println("sonar qube analysis");
+
+
+get("/hello", (req, res) -> "Hello World");
+System.out.println("sonar qube analysis");
+
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(" addition is"+c);
 
     }
 }
