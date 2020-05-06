834f066e93511c151024efcaab83e54d3691fc4d
unknown <mahendrathota131213@gmail.com>
2020-05-06 15:31:18 +0530

    branch1

---
+++ src/main/java/edu/ucsb/cs56/w16/sparkjava/SparkHello - Copy.java
@@ -0,0 +1,24 @@
+package edu.ucsb.cs56.w16.sparkjava;
+
+import static spark.Spark.*;
+import java.io.*; 
+
+import java.util.*;
+public class SparkHello {
+    public static void main(String[] args) {
+        
+get("/hello", (req, res) -> "Hello World");
+System.out.println("sonar qube analysis");
+
+
+get("/hello", (req, res) -> "Hello World");
+System.out.println("sonar qube analysis");
+
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(" addition is"+c);
+// adding some changes
+// add changes and commit
+    }
+}
